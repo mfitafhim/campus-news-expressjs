b97d5ac40f7ac85855e0cb3c0235a6e460dffb80
mfi tafhim <tafhim317@gmail.com>
2022-10-21 20:27:15 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express();
+var cors = require('cors');
+const port = process.env.PORT || 5000
+const categories = require('./data/categories.json');
+const news = require('./data/news.json');
+app.use(cors());
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+app.get('/categories', (req, res) => {
+    res.send(categories);
+})
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news);
+    }
+    else {
+        const categoryNews = news.filter(n => n.category_id === id);
+        res.send(categoryNews);
+    }
+
+})
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
+})
+app.get('/news', (req, res) => {
+    res.send(news);
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
